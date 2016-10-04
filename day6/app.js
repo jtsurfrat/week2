8606758fa20a75cd748b5e1955a567e44a18c096
Jenna <jtsurfrat@hotmail.com>
2016-10-04 09:14:59 -0700

    stuff

--- day6/app.js
+++ day6/app.js
@@ -1,1 +1,29 @@
-console.log('stuff');
+var App;
+(function (App) {
+    angular.module("MyApp", []);
+    var HomeController = (function () {
+        function HomeController() {
+            this.color = "green";
+        }
+        HomeController.prototype.pickColor = function (color) {
+            this.color = color;
+        };
+        HomeController.prototype.addNumbers = function (x, y) {
+            this.anwser = x + y;
+        };
+        HomeController.prototype.subNumbers = function (x, y) {
+            this.anwser = x - y;
+        };
+        HomeController.prototype.multNumbers = function (x, y) {
+            this.anwser = x * y;
+        };
+        HomeController.prototype.divNumbers = function (x, y) {
+            this.anwser = x / y;
+        };
+        HomeController.prototype.modNumbers = function (x, y) {
+            this.anwser = x % y;
+        };
+        return HomeController;
+    }());
+    angular.module("MyApp").controller("homeController", HomeController);
+})(App || (App = {}));
--- day6/app.ts
+++ day6/app.ts
@@ -1,1 +1,52 @@
-console.log('stuff');
+namespace App {
+  //angular module
+  angular.module("MyApp", []);
+
+  //angular.module("MyControllers", []);
+
+// define the HomeController class
+  class HomeController {
+    // defining a public field
+    //public message = "Welcome";
+    //public friends;
+    //public cars;
+    public add;
+    public number1;
+    public number2;
+    public anwser;
+    public color = "green";
+
+    pickColor(color){
+      this.color = color;
+    }
+    addNumbers(x,y){
+      this.anwser = x + y;
+    }
+    subNumbers(x,y){
+      this.anwser = x - y;
+    }
+    multNumbers(x,y){
+      this.anwser = x * y;
+    }
+
+    divNumbers(x,y){
+      this.anwser = x / y;
+    }
+
+    modNumbers(x,y){
+      this.anwser = x % y;
+    }
+
+    constructor(){
+      // this.friends = ['Cartman', "Kyle", "Timmy", "Stan", "Kenny"];
+      // this.cars = [
+      //   { year: 1989, make: "VW", model: "Fox"},
+      //   { year: 1988, make: "VW", model: "TFox"}
+      // ];
+    }
+  }
+  // Add HomeController class to the controller while nameing it HomeController
+  angular.module("MyApp").controller("homeController", HomeController);
+}
+
+// homework make caluclator with angular, takes two pr
